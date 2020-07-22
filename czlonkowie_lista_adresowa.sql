6603464b5dfbd2fd11647b4e7ba5fc122665bab9
oio154 <oio154@gmail.com>
2020-07-22 15:01:48 +0200

    20200722

--- czlonkowie_lista_adresowa.sql
+++ czlonkowie_lista_adresowa.sql
@@ -1,5 +1,6 @@
 select distinct
 -- KLIENT
+k.bkklient_id,
 k.bkklient_nazwa as Nazwisko_imie,
 -- ADRES KLIENTA
 k.bkklient_kod_pocztowy,
@@ -9,7 +10,13 @@ k.bkklient_kod_pocztowy,
 (select bkmiasto_nazwa from bkmiasto where bkmiasto_id = k.bkpoczta_id) as klient_poczta,
 (select bkulica_nazwa from bkulica where bkulica_id = k.bkulica_id) as klient_ulica,
 k.bkklient_numer_posesji || ' / ' || k.BKKLIENT_NUMER_LOKALU as klient_nr_posesji,
+/*
 -- Czy adres klienta w naszych zasobach
+Porównujemy tutaj 2 adresy (stringiem) z bk oraz z inf. Niekiedy te adresy nie sa zgodne
+ze wzglêdu na ró¿ne nazwy ulic.
+*/
+--jacek.admorcount(k.bkklient_id) as adm,
+getADM((select bkmiasto_nazwa from bkmiasto where bkmiasto_id = k.bkmiasto_id), (select bkulica_nazwa from bkulica where bkulica_id = k.bkulica_id), k.bkklient_numer_posesji ) as test_adm3,
 replace(((select bkulica_nazwa from bkulica where bkulica_id = k.bkulica_id) ||k.bkklient_numer_posesji),' ','') as test_adr1,
 case when upper(replace(((select bkulica_nazwa from bkulica where bkulica_id = k.bkulica_id) || ' ' ||k.bkklient_numer_posesji),' ','')) in 
                                                   (select 
@@ -18,6 +25,8 @@ case when upper(replace(((select bkulica_nazwa from bkulica where bkulica_id = k
   then 1
   else 0
   end as test_adr_kli,
+-- ADRES EMAIL (e-mail)
+jacek.getemail(k.bkklient_id) as email,
 -- ADRES KORESP. KLIENTA
 (select bkkraj_nazwa from bkkraj where bkkraj_id = k.BKKORESP_KRAJ_ID) as koresp_kraj,
 k.BKKORESP_KOD_POCZTOWY, 
@@ -26,8 +35,7 @@ k.BKKORESP_KOD_POCZTOWY,
 (select bkulica_nazwa from bkulica where bkulica_id = k.bkkoresp_ulica_id) as koresp_ulica,
 k.BKKORESP_NUMER_POSESJI || ' / ' || k.BKKORESP_NUMER_LOKALU as koresp_posesja
 from bkklient k
-inner join (select * from czlczlonek where czlgrupa_id in (62,64)) czl on czl.bkklient_id = k.bkklient_id --11547
-
-
-
-;
+--tylko czlonkowie
+inner join (select * from czlczlonek where czlgrupa_id in (62,64)) czl on czl.bkklient_id = k.bkklient_id --11541
+--and ROWNUM<30 --dev
+--and jacek.getADM((select bkmiasto_nazwa from bkmiasto where bkmiasto_id = k.bkmiasto_id), (select bkulica_nazwa from bkulica where bkulica_id = k.bkulica_id), k.bkklient_numer_posesji ) not in ('TS','TP','TR','TC','TZ','TK') --dev
